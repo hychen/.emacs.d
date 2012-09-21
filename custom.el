1e1ef2f7f1db7d45d6b114d1349a2cbd06cdd7de
Hsin-Yi Chen (hychen) <ossug.hychen@gmail.com>
2012-09-21 14:55:52 +0800

    set default fonts to monaco

--- custom.el
+++ custom.el
@@ -3,4 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Inconsolata" :foundry "unknown" :slant normal :weight normal :height 128 :width normal)))))
+ '(default ((t (:family "Monaco" :foundry "apple" :slant normal :weight normal :height 113 :width normal)))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
