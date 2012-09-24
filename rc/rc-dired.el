3d92667a2b216a29dc4cc76a00f6a4d3f72abbb3
Hsin-Yi Chen (hychen) <ossug.hychen@gmail.com>
2012-09-24 17:59:47 +0800

    Add comments of dired settings

--- rc/rc-dired.el
+++ rc/rc-dired.el
@@ -6,9 +6,10 @@
 ;; Keywords: comm
 (require 'dired-x)
 
+;; Don't display hidden files, can be toggled by M-o
 (setq dired-omit-files "\\(^\\.[^\\.].*$\\|^\\.\\..+$\\)")
-
 (add-hook 'dired-mode-hook (lambda () (dired-omit-mode 1)))
+;; Highlight Current Line
 (add-hook 'dired-mode-hook 'hl-line-mode)
 
 (provide 'rc-dired)
