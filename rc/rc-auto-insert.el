bf4c893b977bd58130b71e06c2f36068e2045c8d
Hsin-Yi Chen (hychen) <ossug.hychen@gmail.com>
2012-09-24 10:47:38 +0800

    expand auto-inserted content as yasnippet template

--- rc/rc-auto-insert.el
+++ rc/rc-auto-insert.el
@@ -1,10 +1,30 @@
 (require 'autoinsert)
+(rc-require 'yasnippet)
 
 (add-hook 'find-file-hooks 'auto-insert)
 (setq auto-insert-directory (locate-user-emacs-file "skeleton"))
 (setq auto-insert 'other)
 (setq auto-insert-query nil)
 
+;; templates for major-mode
 (define-auto-insert 'python-mode "default.py")
+
+;; expand auto-inserted content as yasnippet template
+(defadvice auto-insert  (around yasnippet-expand-after-auto-insert activate)
+  "expand auto-inserted content as yasnippet templete,
+  so that we could use yasnippet in autoinsert mode"
+  (let ((is-new-file (and (not buffer-read-only)
+                          (or (eq this-command 'auto-insert)
+                              (and auto-insert (bobp) (eobp))))))
+    ad-do-it
+    (let ((old-point-max (point-max)))
+      (when is-new-file
+        (goto-char old-point-max)
+        (yas/expand-snippet (buffer-substring-no-properties (point-min) (point-max)))
+        (delete-region (point-min) old-point-max)
+        )
+      )
+    )
+  )
 
 (provide 'rc-auto-insert)
--- skeleton/default.py
+++ skeleton/default.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# File: `(buffer-name)`
 #
-# Copyright (C) 2012  Hsin-Yi Chen
+# Copyright (C) 2012  `user-full-name`
 
-# Author(s): Hsin-Yi Chen <ossug.hychen@gmail.com>
+# Author(s): `user-full-name` <`user-mail-address`>
 
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -22,4 +22,3 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
-
