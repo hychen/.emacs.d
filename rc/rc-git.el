6436f39bdf3068444e382fc5eb92f78238ca8f19
Hsin-Yi Chen (hychen) <ossug.hychen@gmail.com>
2012-10-11 17:17:09 +0800

    Delete rc-git

--- rc/rc-git.el
+++
@@ -1,8 +0,0 @@
-(rc-require 'magit)
-
-(eval-after-load 'magit
-  '(progn
-     (set-face-foreground 'magit-diff-add "green4")
-     (set-face-foreground 'magit-diff-del "red3")))
-
-(provide 'rc-git)
