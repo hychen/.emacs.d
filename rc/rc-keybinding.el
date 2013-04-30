9c689f0fede54923f9a2d751074cb1b1715aa628
Hsin-Yi Chen (hychen) <ossug.hychen@gmail.com>
2013-04-30 18:09:07 +0800

    add mutiple cursor binding.

--- rc/rc-keybinding.el
+++ rc/rc-keybinding.el
@@ -4,11 +4,19 @@
 
 ;; Author: hychen <ossug.hychen@gmail.com>
 (package-require 'ace-jump-mode)
+(package-require 'multiple-cursors)
 (require 'ace-jump-mode)
+(require 'multiple-cursors)
+
+;; Multiple cursors for Emacs.  https://github.com/magnars/multiple-cursors.el
+;; When you have an active region that spans multiple lines, the following will add a cursor to each line:
+(global-set-key (kbd "C-3") 'mc/edit-lines)
+;; When you want to add multiple cursors not based on continuous lines, but based on keywords in the buffer, use:
+(global-set-key (kbd "C-4") 'mc/mark-all-like-this)
 
 ;; enabling fast/direct cursor movement in current view
 ;; ref: http://www.emacswiki.org/emacs/AceJump
-(define-key global-map (kbd "C-3") 'ace-jump-mode)
+(global-set-key (kbd "C-;") 'ace-jump-mode)
 
 ;; Use regex searches by default.
 (global-set-key (kbd "C-s") 'isearch-forward-regexp)
@@ -41,5 +49,5 @@
         (indent-according-to-mode)))
 
 (global-set-key [S-return]   'open-next-line)
-
+(global-unset-key-map (kbd "C-space")
 (provide 'rc-keybinding)
