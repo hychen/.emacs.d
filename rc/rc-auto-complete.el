e03a4449e514cc8d9b3a048bbbd115e976466c00
Hsin-Yi Chen (hychen) <ossug.hychen@gmail.com>
2013-05-05 17:57:14 +0800

    auto insert pairs and enable auto-dwim.

--- rc/rc-auto-complete.el
+++ rc/rc-auto-complete.el
@@ -2,6 +2,25 @@
 (require 'auto-complete-config)
 
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+;; Do What I Mean mode
+(setq ac-dwim t)
 (ac-config-default)
+
+;; -------------------
+;; Pairing parentheses
+;; -------------------
+
+;; ref: https://github.com/gabrielelanaro/emacs-for-python/blob/master/epy-completion.el
+;; All languages:
+(setq skeleton-pair t)
+(global-set-key "(" 'skeleton-pair-insert-maybe)
+(global-set-key "[" 'skeleton-pair-insert-maybe)
+(global-set-key "{" 'skeleton-pair-insert-maybe)
+(global-set-key "\"" 'skeleton-pair-insert-maybe)
+
+;; Just python
+(add-hook 'python-mode-hook 
+	  (lambda () 
+	    (define-key python-mode-map "'" 'skeleton-pair-insert-maybe)))
 
 (provide 'rc-auto-complete)
