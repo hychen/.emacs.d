cdf8eec46316efdc32860fb042fac54685c3ed5c
Hsin-Yi Chen (hychen) <ossug.hychen@gmail.com>
2013-05-05 16:14:25 +0800

    python: use ipython as default shell.

--- rc/rc-python.el
+++ rc/rc-python.el
@@ -1,9 +1,27 @@
 
+;; ref: 
+;; - https://github.com/gabrielelanaro/emacs-for-python
+;; - http://www.jesshamrick.com/2012/09/18/emacs-as-a-python-ide/
 
-(add-hook 'python-mode-hook
-      (lambda ()
-	;; Insert operators with surrounding spaces smartly.
-	(smart-operator-mode)
-	))
+(eval-after-load 'python
+  '(progn
+     ;; use IPython
+     (setq
+      python-shell-interpreter "ipython"
+      python-shell-interpreter-args ""
+      python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+      python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+      python-shell-completion-setup-code
+      "from IPython.core.completerlib import module_completion"
+      python-shell-completion-module-string-code
+      "';'.join(module_completion('''%s'''))\n"
+      python-shell-completion-string-code
+      "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+
+     ;; Insert operators with surrounding spaces smartly.
+     (smart-operator-mode)
+     ))
 
 (provide 'rc-python)
+
+
