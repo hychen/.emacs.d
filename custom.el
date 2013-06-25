02f8f5bbca50aefc4fca06dd0af2571d528df31a
Hsin-Yi Chen (hychen) <ossug.hychen@gmail.com>
2013-06-25 12:20:24 +0800

    nicklook chinese font setting.

--- custom.el
+++ custom.el
@@ -4,13 +4,16 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Monaco" :foundry "apple" :slant normal :weight normal :height 113 :width normal)))))
+(set-fontset-font (frame-parameter nil 'font)
+    'unicode '("WenQuanYi Zen Hei" . "unicode-bmp") 'nil 'append)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
- '(org-agenda-files (quote ("~/Ubuntu One/org/dayplanner/work.org" "~/Ubuntu One/org/dayplanner/work-fo.org" "~/Ubuntu One/org/dayplanner/work-st.org" "~/Ubuntu One/org/dayplanner/org.org" "~/Ubuntu One/org/dayplanner/todo.org" "~/Ubuntu One/org/dayplanner/refile.org" )))
+ '(font-use-system-font t)
+ '(org-agenda-files (quote ("~/Ubuntu One/org/dayplanner/work.org" "~/Ubuntu One/org/dayplanner/work-fo.org" "~/Ubuntu One/org/dayplanner/work-st.org" "~/Ubuntu One/org/dayplanner/org.org" "~/Ubuntu One/org/dayplanner/todo.org" "~/Ubuntu One/org/dayplanner/refile.org")))
  '(show-paren-mode t)
  '(tool-bar-mode nil)
  '(virtualenv-root "/home/hychen/.virtualenv/"))
