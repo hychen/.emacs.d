eb9c144e745b7200dd2fffb6419664651eb4eb9b
陳信屹 <ossug.hychen@gmail.com>
2015-01-17 11:16:03 +0800

    larger font.

--- custom.el
+++ custom.el
@@ -3,6 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Monaco" :foundry "apple" :slant normal :weight normal :height 143 :width normal)))))
+ '(default ((t (:family "Monaco" :foundry "apple" :slant normal :weight normal :height 163 :width normal)))))
 (set-fontset-font (frame-parameter nil 'font)
     'unicode '("WenQuanYi Zen Hei" . "unicode-bmp") 'nil 'append)
