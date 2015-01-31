1d420a42b13b0859ed84e44142b9eb65e6723b09
陳信屹 <ossug.hychen@gmail.com>
2015-01-31 14:46:07 +0800

    auto inserting org-mode header.

--- rc/rc-autoinsert.el
+++ rc/rc-autoinsert.el
@@ -1,6 +1,8 @@
 ;;; rc-autoinsert.el
 (require 'autoinsert)
 
+(add-hook 'find-file-hooks 'auto-insert)
+
 ; Insert if possible, but mark as unmodified.                                        
 (setq auto-insert 'other)
 
@@ -10,7 +12,12 @@
                                         
 (add-to-list 'auto-insert-alist
              '(("\\.org\\'" . "Org-mode header")
-               "#+TITLE:"
+               "Short description: "
+               "#+TITLE:" (file-name-nondirectory (file-name-sans-extension (buffer-file-name))) "\n"
+               "#+AUTHOR:" (user-full-name) "\n"
+               "#+OPTIONS: H:2 num:t toc:nil\n"
+               "#+OPTIONS: ^:nil\n"
+               "#+OPTIONS: <:nil todo:nil *:t ^:{} @:t ::t |:t TeX:t\n"
                ))
 
 (provide 'rc-autoinsert)
